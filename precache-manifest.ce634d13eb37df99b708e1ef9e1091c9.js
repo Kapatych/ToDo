67649f3ad84801929218c23b8e40f86e04bfdb99
Kapatych <onetruestory@ya.ru>
2020-02-12 15:15:31 +0300

    Updates

---
+++ precache-manifest.ce634d13eb37df99b708e1ef9e1091c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93e663bb766a54dc248c9bd14064a24",
+    "url": "/Kapatych/ToDo/index.html"
+  },
+  {
+    "revision": "e31ace8a2b55fab405a5",
+    "url": "/Kapatych/ToDo/static/css/main.948e1b14.chunk.css"
+  },
+  {
+    "revision": "815210066c88799278d8",
+    "url": "/Kapatych/ToDo/static/js/2.6f010cd3.chunk.js"
+  },
+  {
+    "revision": "e31ace8a2b55fab405a5",
+    "url": "/Kapatych/ToDo/static/js/main.78dff3ad.chunk.js"
+  },
+  {
+    "revision": "73394a95d9b01ac652b2",
+    "url": "/Kapatych/ToDo/static/js/runtime-main.decc8bbb.js"
+  }
+]);
